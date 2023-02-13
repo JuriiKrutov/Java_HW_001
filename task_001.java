f1747355e77b1b4b82a6f60d48a4e52f4864ef38
JuriiKrutov <juriikrutov@gmail.com>
2023-02-13 13:23:29 +0300

    написал функцию вычесления треугольного числа

--- task_001.java
+++ task_001.java
@@ -1,11 +1,16 @@
 package homework_001;
 
+import java.util.Scanner;
 /**
  * task_001
  */
 public class task_001 {
 
     public static void main(String[] args) {
-        
+
+    }
+
+    static int triangle(int num) {
+            return num = num * (num + 1) / 2;
     }
 }
