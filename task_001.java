4a911c9f9cedcaf57f0c318a7ae59cfe14a9ed3e
JuriiKrutov <juriikrutov@gmail.com>
2023-02-13 13:25:07 +0300

    закончил написание программы

--- task_001.java
+++ task_001.java
@@ -7,6 +7,11 @@ import java.util.Scanner;
 public class task_001 {
 
     public static void main(String[] args) {
+            
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int num = sc.nextInt();
+        System.out.println(triangle(num));
 
     }
 
